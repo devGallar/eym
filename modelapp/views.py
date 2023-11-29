7ae69d98a81c735c07702b63ffb39e7494cd817a
tallerapp <grupotallerapp@gmail.com>
2023-11-29 19:11:38 -0300

    Ver productos

--- modelapp/views.py
+++ modelapp/views.py
@@ -4,22 +4,6 @@ from modelapp.models import *
 
 
 def index(request):
-
-    return render(request,'index.html')
-def verProductos(request):
     productos = Producto.objects.all();
-    dict = {"productos": productos}
-    return render(request,"verproductos.html",dict)
-
-def frmCompra(request, id):
-    producto = Producto.objects.get(id=id)  
-    dict = {"producto": {
-        "nombre": producto.nombre,
-            "imagen_url": producto.imagen.url,
-            "precio": producto.precio,
-            "stock": producto.stock,
-            "descripcion": producto.descripcion,
-            "categoria": producto.categoria.nombre
-    }}
-
-    return render(request, "frmcompra.html", dict)
+    context = {"productos": productos}
+    return render(request,'index.html',context)
