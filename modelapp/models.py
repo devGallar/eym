27734941be7611651fa13c9c88f1069d67c6f496
tallerapp <grupotallerapp@gmail.com>
2023-11-30 11:34:30 -0300

    Creacion de modelo pedidos

--- modelapp/models.py
+++ modelapp/models.py
@@ -27,3 +27,27 @@ class Cliente(models.Model):
     
     def __str__(self):
         return self.dni
+    
+class Pedido(models.Model):
+    
+    ESTADO_CHOICES = (
+        ('0','Solicitado'),
+        ('1','Pagado')
+    )
+    
+    cliente = models.ForeignKey(Cliente,on_delete=models.RESTRICT)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    nro_pedido = models.CharField(max_length=20,null=True)
+    monto_total = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    estado = models.CharField(max_length=1,default='0',choices=ESTADO_CHOICES)
+    
+    def __str__(self):
+        return self.nro_pedido
+class PedidoDetalle(models.Model):
+    pedido = models.ForeignKey(Pedido,on_delete=models.RESTRICT)
+    producto = models.ForeignKey(Producto,on_delete=models.RESTRICT)
+    cantidad = models.IntegerField(default=1)
+    subtotal = models.DecimalField(max_digits=10,decimal_places=2)
+    
+    def __str__(self):
+        return self.producto.nombre
