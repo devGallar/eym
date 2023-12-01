df9276b559c11daea4d8ee05098ccf441f5ec9dd
tallerapp <grupotallerapp@gmail.com>
2023-11-30 22:23:11 -0300

    base de datos en la nube

--- eym/settings.py
+++ eym/settings.py
@@ -67,11 +67,13 @@ WSGI_APPLICATION = 'eym.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eymdb',
+        'USER': 'eymadmin',
+        'PASSWORD': 'Inacap.2023',
+        'HOST': '34.176.151.115',
+        'PORT': '5432'}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
